58f1a7ff6745f4c8dd4ca08480bb4cd869a49af7
Ruban_S <rubans5400@gmail.com>
2025-10-22 10:42:36 +0530

    feat: undefined_models added

---
+++ lib/models/undefined_models.dart
@@ -0,0 +1,11 @@
+// The data you need to store
+const Map<String, bool> initialReferenceStatus = {
+  'ST-S1': false,
+  'ST-S3': false,
+  'ST-S4': true,
+  'ST-S5': true,
+  'ST-S6': true,
+  'ST-S7': false,
+  'ST-S8': false,
+  'ST-S2': false,
+};
